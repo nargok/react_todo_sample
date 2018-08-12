079a0523faaaa4a474da60ae889b8d30e2b48ecf
Ryoichi Gokan <nargok@gmail.com>
2018-08-12 22:19:45 +0900

    入力UIとタスク一覧を表示

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Todo extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      inputValue: "",
+      tasks: [
+        { title: "デフォルトタスク", done: false }
+      ]
+    }
+  }  
+
+  render() {
+    const items = this.state.tasks.map((task, index) => {
+      return <li key={index}>{task.title}</li>
+    })
+
+    return(
+      <React.Fragment>
+        <div>
+          <input type="text" />
+          <button>Add</button>
+          <button>Reset</button>
+        </div>
+        <ul>
+          {items}
+        </ul>
+      </React.Fragment>
+    )
+  }
+}
+
+ReactDOM.render(<Todo />, document.getElementById('root'));
 registerServiceWorker();
